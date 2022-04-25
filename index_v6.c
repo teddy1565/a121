cd4a6f582435f90f0009da752df8c94355ed40c4
teddy1565 <teddy1565@gmail.com>
2022-04-25 21:37:51 +0800

    參考作法 埃拉

---
+++ index_v6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int Find_the_Primes(int x){
+    if(x<=1) return 0;
+    for(int i=2;i*i<=x;i++)
+        if(x%i==0) return 0;
+    return 1;
+}
+int main(){
+    int a,b;
+    printf("%d\n", Find_the_Primes(15));
+    while(scanf("%d %d",&a,&b)!=EOF){
+        int sum=0;
+        for(int i=a;i<=b;i++){
+            if(Find_the_Primes(i)==1) sum++;
+        }
+        printf("%d\n",sum);
+    }
+    return 0;
+}
