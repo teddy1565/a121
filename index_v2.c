b4a33a7b1a5341648b72b903855cc7dd7636ebd9
teddy1565 <teddy1565@gmail.com>
2022-04-25 20:23:54 +0800

    simple

---
+++ index_v2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int checkPrime(int n) {
+    for (int i=3;i<=n;i+=2) {
+        if (i == n) {
+            return 1;
+        } else if (i%n == 0 && i!=n) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(void) {
+    int n,m;
+    while(scanf("%d %d", &n, &m) != EOF) {
+        int counter=0;
+        if((n&1) == 0) {
+            if (n == 2) {
+                counter++;
+            }
+            n++;
+        }
+        for (int i=n;i<=m;i+=2) {
+            if (checkPrime(i)) {
+                counter++;
+            }
+        }
+        printf("%d\n", counter);
+    }
+    return 0;
+}
