39c90b25d09ef3c07848a1e187074fa3cfe67057
teddy1565 <teddy1565@gmail.com>
2022-04-27 00:47:19 +0800

    self training

--- index_v7.c
+++ index_v7.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int checkPrime(int x) {
+    if (x <= 1) {
+        return 0;
+    }
+    for (int i=2; i*i <= x; i++) {
+        if (x%i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 int main(void) {
+    int n,m;
+    while(scanf("%d %d",&n ,&m) != EOF) {
+        int counter = 0;
+        if ((n&1) == 0) {
+            if (n == 2) {
+                counter++;
+            }
+            n++;
+        }
+        for (int i=n; i<=m; i+=2) {
+            if (checkPrime(i)) {
+                counter++;
+            }
+        }
+        printf("%d\n", counter);
+    }
     return 0;
 }
